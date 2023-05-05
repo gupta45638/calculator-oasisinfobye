03ef951b84c9daea192a291cbf378d9af133e75a
Nandini gupta <120970314+gupta45638@users.noreply.github.com>
2023-05-05 12:38:44 -0700

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,16 @@
+let string = '';
+const buttons = document.querySelectorAll('.button');
+
+Array.from(buttons).forEach((button) => {
+  button.addEventListener('click', (e) => {
+    if (e.target.innerHTML === '=') {
+      string = eval(string);
+      document.querySelector('input').value = string;
+    } else {
+      console.log(e.target);
+      string += e.target.innerHTML;
+      document.querySelector('input').value = string;
+    }
+  });
+});
+
